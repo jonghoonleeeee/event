8e49bca922ab7ab84f263215efb9957c614c1abb
memezawa00st <memezawa@startup-technology.com>
2021-05-25 16:53:53 +0900

    fix

--- js/script.js
+++ js/script.js
@@ -13,13 +13,13 @@ $(window).scroll(function() {
     $(".js-middle").addClass('active')
   }
   //画像エリアの位置を取得
-  jsMainBottomScrollTopPoint = $('.js-bottom');.offset().top;
+  bottomTop = $('.js-bottom').offset().top;
   //windowの高さの取得
   mainTxtHeight = $(window).height();
   //画像エリアのが表示されるスクロール量の計算
-  scrollEventPoint = jsMainBottomScrollTopPoint - mainTxtHeight;
+  scrollEventPoint = bottomTop - mainTxtHeight + 200;
 
   if (scrollPoint >= scrollEventPoint) {
-    $('.js-bottom');.addClass('active');
+    $('.js-bottom').addClass('active');
   }
 });
